d017ca02d616adcc3ff2e56eea1678dd2960c695
VitorDiass <vitordias28@hotmail.com>
2022-03-11 23:48:29 +0000

    design

--- src/App.tsx
+++ src/App.tsx
@@ -1,22 +1,28 @@
-import React from 'react';
+import React from "react";
 import MainMenu from "./component/mainMenu";
-import { Button, Divider, Input, Segment } from 'semantic-ui-react'
+import { Button, Divider, Input, Segment } from "semantic-ui-react";
+
 
 const App = () => {
   return (
     <>
-    <div className="h-screen bg-backimage bg-cover bg-no-repeat">
-    {/*   <div className="flex justify-center">STATUS BAR</div> */}
-      <div className="h-52 flex justify-center items-center">
-        <span className="text-white main-title text-glow-white">
-          PEXAGY STELLAR
-        </span>
+      <div className="min-h-screen bg-backimage bg-cover bg-no-repeat">
+        {/*   <div className="flex justify-center">STATUS BAR</div> */}
+        <div className="h-52 flex justify-center items-end">
+          <div className="flex-col">
+            <span className="text-white main-title text-glow-white">
+              PEXAGY STELLAR
+            </span>
+            <div className="text-center text-lg mt-10">
+              A multi purpose fansite made for players and managers of Pegaxy
+            </div>
+          </div>
+        </div>
+        <div className="mx-20">
+          <div className="ui divider main-divider"></div>
+        </div>{" "}
+        <MainMenu />
       </div>
-      <div className='mx-20'>
-        <div className='ui divider main-divider'></div>
-      </div>
-    <MainMenu></MainMenu>
-    </div>
     </>
   );
 };
--- src/component/mainMenu.tsx
+++ src/component/mainMenu.tsx
@@ -1,11 +1,30 @@
+import { Card, Icon, Image } from 'semantic-ui-react'
+import CardComponent from "./card";
+import { MdDashboard } from "react-icons/md";
+import {IoPersonSharp} from 'react-icons/io5'
+import { GiPegasus } from "react-icons/gi";
+import { GoTools } from "react-icons/go";
+
 const MainMenu = () => {
   return (
-    <div className="flex justify-evenly">
-     {/*  <div className="header text-glow-white">GENERAL</div>
-      <div className="header text-glow-white">PEGA</div>
-      <div className="header text-glow-white">USER</div>
-      <div className="header text-glow-white">TICKET</div> */}
-    </div>
+    <>
+      <div className="flex justify-evenly mt-20">
+        <CardComponent cardTitle='GENERAL' cardColor='var(--color-light-blue)' linearBgTop='var(--color-light-blue-025)' linearBgBottom='var(--color-light-blue-001)'>
+          <MdDashboard className='text-4xl'/>
+        </CardComponent>
+        <CardComponent cardTitle='USER' cardColor='var(--color-orange)' linearBgTop='var(--color-orange-025)' linearBgBottom='var(--color-orange-001)'>
+          <IoPersonSharp className='text-4xl'/>
+        </CardComponent>
+      </div>
+      <div className="flex justify-evenly mt-16">
+        <CardComponent cardTitle='PEGA' cardColor='var(--color-pink)' linearBgTop='var(--color-pink-025)' linearBgBottom='var(--color-pink-001)'>
+          <GiPegasus className='text-4xl'/>
+        </CardComponent>
+        <CardComponent cardTitle='TOOLS' cardColor='var(--color-green)' linearBgTop='var(--color-green-025)' linearBgBottom='var(--color-green-001)'>
+          <GoTools className='text-3xl'/>
+        </CardComponent>
+      </div>
+    </>
   );
 };
 
