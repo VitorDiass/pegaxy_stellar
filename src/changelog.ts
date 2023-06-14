b9984b9a32ecbdd41f6f5b0442bc221fca22fdc3
VitorDiass <vitordias28@hotmail.com>
2023-06-14 16:14:37 +0100

    changelog

--- src/changelog.ts
+++ src/changelog.ts
@@ -1,27 +1,27 @@
 export const Changelog : any = [
     {
-        date : '15/04/2022',
+        /* date : '15/04/2022', */
         textChanges : [],
         version : '0.0.1 beta - release',
     },
     {
-        date : '16/04/2022',
+        /* date : '16/04/2022', */
         textChanges : ['Added rarity field to user owned pega table','Reduced main page png size'],
         version : '0.0.2 beta',
     },
     {
-        date : '19/04/2022',
+        /* date : '19/04/2022', */
         textChanges : ['Breedable logic now uses "canBreedAt" field instead of calculations based on "lastTimeBreed"','Fix responsiveness problems','Shorten wallet address (ex: 0xFA231...43213)'],
         version : '0.0.3 beta',
     },
     {
-        date : '21/04/2022',
+        /* date : '21/04/2022', */
         textChanges : ['Added changelog page','Added changelog link to footer','"Breedable" and "Raceable" now shows the time remaining for the horse to be able to breed', 'Added "Total Races" indicador to table\'s header'],
         version : '0.0.4 beta',
     },
     {
-        date : '08/05/2022',
-        textChanges : ['Added pega lookup page - id, name, gender, breed type, rarity, born time, wr, wins, loses, avg vis/day, avg vis/race, parent\'s id, status, enegy, breed count, service', 'Added link from wallet pega table to pega lookup', 'Added pega id input to app main page'],
+        /* date : '08/05/2022', */
+        textChanges : ['Added pega lookup page - id, name, gender, breed type, rarity, born time, wr, wins, loses, avg vis/day, avg vis/race, parent\'s id, status, enegy, breed count, service', 'Added link from wallet pega table to pega lookup', 'Added pega id input to app\'s main page'],
         version : '0.0.5 beta',
     }
 ]
